85f997b2437a1da9257215314784a959b6edc82f
IngSebastianBiach <sebukpo@hotmail.com>
2021-03-25 01:10:11 -0300

    Funciona

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,21 +17,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun calculateTip() {
+    private fun calculateTip() {
 
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
 
-        val selectId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when(selectId) {
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15}
 
            var tip = tipPercentage * cost
-            val roundUp = binding.roundUpSwitch.isChecked
 
-        if (roundUp) {
+
+        if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
 
